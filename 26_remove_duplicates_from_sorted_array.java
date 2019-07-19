e5e6fae1327be2b0605e188d596fd7e2db60359c
Xudong Li <lixudong0402@gmail.com>
2019-07-18 21:50:50 -0700

    lc26 added

---
+++ 26_remove_duplicates_from_sorted_array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[size]) {
+                nums[++size] = nums[i]; 
+            }
+        }
+        
+        return size + 1;
+        
+    }
+}
