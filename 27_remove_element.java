266b317d7bdd1f5d14e5668e24dd3195bdeab625
Xudong Li <lixudong0402@gmail.com>
2019-07-20 13:26:18 -0700

    lc27 added

---
+++ 27_remove_element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            while (left <= right && nums[left] != val) {
+                left++;
+            }
+            while (left <= right && nums[right] == val) {
+                right--;
+            }
+            
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        
+        return left;
+    }
+}
