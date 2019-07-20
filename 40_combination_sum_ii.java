0990563f7153781248e6b185e96d275fb078013e
Xudong Li <lixudong0402@gmail.com>
2019-07-20 15:40:50 -0700

    lc40 added

---
+++ 40_combination_sum_ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    private List<List<Integer>> result = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) {
+            return result;
+        }
+        
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, new ArrayList<Integer>());
+        
+        return result;
+    }
+    
+    private void helper(int[] candidates, int start, int target, List<Integer> curr) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(curr));
+            return;
+        }
+        
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+            
+            if (i != start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            curr.add(candidates[i]);
+            helper(candidates, i + 1, target - candidates[i], curr);
+            curr.remove(curr.size() - 1);
+        }
+        
+        return;
+    }
+}
