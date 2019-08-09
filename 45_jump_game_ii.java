8a931baf0daf2c0ef8e6e4d70a9b8a5ea1778fc5
Xudong Li <lixudong0402@gmail.com>
2019-08-09 13:50:51 -0700

    lc45 added

---
+++ 45_jump_game_ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int start = 0, end = 0, jumps = 0;
+        while (end < nums.length - 1) {
+            jumps++;
+            int farthest = end;
+            for (int i = start; i <= end; i++) {
+                if (nums[i] + i > farthest) {
+                    farthest = nums[i] + i;
+                }
+            }
+            start = end + 1;
+            end = farthest;
+        }
+        
+        return jumps;
+    }
+}
