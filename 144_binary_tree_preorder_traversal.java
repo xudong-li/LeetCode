0d519713f396e1204e8bbb2020b16ba8d39268a2
Xudong Li <lixudong0402@gmail.com>
2019-08-19 22:42:10 -0700

    lc144 added

---
+++ 144_binary_tree_preorder_traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            result.add(temp.val);
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+        
+        return result;
+        
+    }
+}
