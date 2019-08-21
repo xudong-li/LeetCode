9e387b1b73babc41106239cc17f3a86eb7f1d83a
Xudong Li <lixudong0402@gmail.com>
2019-08-20 21:21:24 -0700

    lc154 added

---
+++ 154_find_minimum_in_rotated_sorted_array_ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == nums[end]) {
+                end--;
+            } else if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        return Math.min(nums[start], nums[end]);
+    }
+}
