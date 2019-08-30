6ba482b03f2c9e99f4226e467b17e063a4bf9aa2
Xudong Li <lixudong0402@gmail.com>
2019-08-29 21:42:47 -0700

    lc95 added

---
+++ 95_unique_binary_search_tree_ii.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n < 1) {
+            return new ArrayList<TreeNode>();
+        }
+        
+        return helper(1, n);
+    }
+    
+    private List<TreeNode> helper(int start, int end) {
+        
+        List<TreeNode> result = new ArrayList<>();
+        
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = helper(start, i - 1);
+            List<TreeNode> right = helper(i + 1, end);
+            
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    result.add(root);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
