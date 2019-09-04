5bcc91eb78d46376680081e68a347870eee84689
Xudong Li <lixudong0402@gmail.com>
2019-09-03 22:53:58 -0700

    lc134 added

---
+++ 134_gas_station.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = 0;
+        int costSum = 0;
+        int start = 0;
+        int tank = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            gasSum += gas[i];
+            costSum += cost[i];
+            tank += gas[i] - cost[i];
+            
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0; 
+            }
+        }
+        
+        if (gasSum < costSum) {
+            return -1;
+        }
+        return start;
+    }
+}
