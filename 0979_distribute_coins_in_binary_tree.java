4475492909990ece5d56c25e0821542a8b5c344b
Xudong Li <lixudong0402@gmail.com>
2019-09-12 20:48:06 -0700

    lc979 added

---
+++ 0979_distribute_coins_in_binary_tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int result = 0;
+    
+    public int distributeCoins(TreeNode root) {
+        helper(root);
+        return result;
+    }
+    
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        result += Math.abs(left) + Math.abs(right);
+        
+        return root.val + left + right - 1;
+    }
+}
