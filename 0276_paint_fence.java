25c396770e3489e1515780b5a4d6bb814dedacb4
Xudong Li <lixudong0402@gmail.com>
2020-06-09 21:58:45 -0700

    lc276 added

---
+++ 0276_paint_fence.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/paint-fence/discuss/178010/The-only-solution-you-need-to-read
+class Solution {
+    public int numWays(int n, int k) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (n == 1) {
+            return k;
+        }
+        
+        if (n == 2) {
+            return k * k;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = k;
+        dp[2] = k * k;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1);
+        }
+        
+        return dp[n];
+    }
+}
