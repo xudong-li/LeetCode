d6a821a9be0ea3bbad0b2bb46ba994e6b9100ad5
Xudong Li <lixudong0402@gmail.com>
2020-08-11 22:57:27 -0700

    lc41 added

--- 0041_first_missing_positive.java
+++ 0041_first_missing_positive.java
@@ -2,13 +2,12 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         boolean foundOne = false;
-        int maxPossible = n + 1;
         
         for (int i = 0; i < n; i++) {
             if (nums[i] == 1) {
                 foundOne = true;
             }
-            if (nums[i] >= maxPossible || nums[i] <= 0) {
+            if (nums[i] > n || nums[i] <= 0) {
                 nums[i] = 1;
             }
         }
@@ -17,24 +16,18 @@ class Solution {
         }
         
         for (int i = 0; i < n; i++) {
-            int a = Math.abs(nums[i]);
-            if (a == n) {
-                nums[0] = - Math.abs(nums[0]);
-            } else {
-                nums[a] = - Math.abs(nums[a]);
-            }   
+            int a = Math.abs(nums[i]) - 1;
+            nums[a] = - Math.abs(nums[a]);
+             
         }
         
         for (int i = 1; i < n; i++) {
             if (nums[i] > 0) {
-                return i;
+                return i + 1;
             }
         }
         
-        if (nums[0] > 0) {
-            return n;
-        }
-        
+        // this means there is no positive num, the result is n + 1
         return n + 1;
     }
 }
