4af9b4b1d3329327017cd9b61c3d70f7bd3c0121
Xudong Li <lixudong0402@gmail.com>
2020-08-24 21:19:33 -0700

    lc32 added

---
+++ 0032_Longest_Valid_Parentheses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+                if (left == right) {
+                    maxLength = Math.max(maxLength, 2 * right);
+                } else if (right > left) {
+                    left = 0;
+                    right = 0;
+                }
+            }
+        }
+        
+        left = 0;
+        right = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')') {
+                right++;
+            } else {
+                left++;
+                if (left == right) {
+                    maxLength = Math.max(maxLength, 2 * left);
+                } else if (left > right) {
+                    left = 0;
+                    right = 0;
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+}
