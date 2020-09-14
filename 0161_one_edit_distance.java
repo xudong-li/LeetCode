e56ccc7b765f7b41c68b22e21bd014ade80ce830
Xudong Li <lixudong0402@gmail.com>
2020-09-13 22:30:56 -0700

    lc161 added

---
+++ 0161_one_edit_distance.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        int ns = s.length();
+        int nt = t.length();
+        
+        // ensure s is shorter than t
+        if (ns > nt) {
+            return isOneEditDistance(t, s);
+        }
+        
+        if (nt - ns > 1) {
+            return false;
+        }
+        
+        for (int i = 0; i < ns; i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (ns == nt) {
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                } else {
+                    return s.substring(i).equals(t.substring(i + 1));
+                }
+            }
+        }
+        
+        return (ns + 1 == nt);
+    }
+}
